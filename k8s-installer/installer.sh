0faaca0957664de9adcd4b01b643e3739157d309
Parav Pandit <parav@mellanox.com>
2018-06-21 13:07:34 -0500

    installer: Avoid overwriting files if they are already present.
    Signed-off-by: Parav Pandit <parav@mellanox.com>

--- k8s-installer/installer.sh
+++ k8s-installer/installer.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cp -f /bin/sriov /host-cni-bin/
-cp -f /bin/fixipam /host-cni-bin/
+if [ ! -e /host-cni-bin/sriov ]; then
+	cp /bin/sriov /host-cni-bin/
+fi
 
-cp -f /installer/10-sriov-cni.conf /host-cni-etc/
+if [ ! -e /host-cni-bin/fixipam ]; then
+	cp -f /bin/fixipam /host-cni-bin/
+fi
+
+if [ ! -e /host-cni-etc/10-sriov-cni.conf ]; then
+	cp /installer/10-sriov-cni.conf /host-cni-etc/
+fi
