07590d1a71e863ece9288ff46d98d2605f627983
mamduhala <mamduhala@mellanox.com>
2018-10-14 13:14:48 +0000

    Fix sriov Operations unit test
    issue: 1535154
    Change-Id: I5cb74ac7ea270a7771bfa86b6ea2fb31b5642847
    Signed-off-by: mamduhala <mamduhala@mellanox.com>

--- src/sriov/sriov_test.go
+++ src/sriov/sriov_test.go
@@ -14,10 +14,14 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-const MASTER_NAME = "eth1"
+var MASTER_NAME = os.Getenv("PF_INTERFACE")
 
 var _ = Describe("sriov Operations", func() {
 	var originalNS ns.NetNS
+
+	BeforeSuite(func() {
+		Expect(MASTER_NAME).ToNot(Equal(""))
+	})
 
 	BeforeEach(func() {
 		var err error
@@ -44,80 +48,81 @@ var _ = Describe("sriov Operations", func() {
 	AfterEach(func() {
 		Expect(originalNS.Close()).To(Succeed())
 	})
+	Describe("sriov Operations", func() {
+		It("configures and deconfigures a sriov link with ADD/DEL", func() {
+			const IFNAME = "sriovl0"
 
-	It("configures and deconfigures a sriov link with ADD/DEL", func() {
-		const IFNAME = "sriovl0"
+			conf := fmt.Sprintf(`{
+				"name": "mynet",
+				"type": "sriov",
+				"if0": "%s",
+				"mac":"66:77:88:99:aa:bb",
+				"vf": 1,
+				"ipam": {
+					"type": "fixipam",
+					"subnet": "192.168.1.0/24",
+					"gateway": "192.168.1.1"
+				}
+			}`, MASTER_NAME)
 
-		conf := fmt.Sprintf(`{
-    "name": "mynet",
-    "type": "sriov",
-    "master": "%s",
-    "mac":"66:77:88:99:aa:bb",
-    "vf": 1,
-    "ipam": {
-        "type": "fixipam",
-        "subnet": "192.168.1.0/24",
-        "gateway": "192.168.1.1"
-    }
-}`, MASTER_NAME)
-
-		targetNs, err := ns.NewNS()
-		Expect(err).NotTo(HaveOccurred())
-		defer targetNs.Close()
-
-		args := &skel.CmdArgs{
-			ContainerID: "dummy",
-			Netns:       targetNs.Path(),
-			IfName:      IFNAME,
-			StdinData:   []byte(conf),
-		}
-
-		// Make sure sriov link exists in the target namespace
-		err = originalNS.Do(func(ns.NetNS) error {
-			defer GinkgoRecover()
-			os.Setenv("CNI_ARGS", "IP=192.168.1.3")
-			defer func() {
-				os.Unsetenv("CNI_ARGS")
-			}()
-			_, err := testutils.CmdAddWithResult(targetNs.Path(), IFNAME, func() error {
-				return cmdAdd(args)
-			})
+			targetNs, err := ns.NewNS()
 			Expect(err).NotTo(HaveOccurred())
-			return nil
-		})
-		Expect(err).NotTo(HaveOccurred())
+			defer targetNs.Close()
 
-		// Make sure sriov link exists in the target namespace
-		err = targetNs.Do(func(ns.NetNS) error {
-			defer GinkgoRecover()
+			args := &skel.CmdArgs{
+				ContainerID: "dummy",
+				Netns:       targetNs.Path(),
+				IfName:      IFNAME,
+				StdinData:   []byte(conf),
+			}
 
-			link, err := netlink.LinkByName(IFNAME)
+			// Make sure sriov link exists in the target namespace
+			err = originalNS.Do(func(ns.NetNS) error {
+				defer GinkgoRecover()
+				os.Setenv("CNI_ARGS", "IP=192.168.1.3")
+				defer func() {
+					os.Unsetenv("CNI_ARGS")
+				}()
+				_, err := testutils.CmdAddWithResult(targetNs.Path(), IFNAME, func() error {
+					return cmdAdd(args)
+				})
+				Expect(err).NotTo(HaveOccurred())
+				return nil
+				})
 			Expect(err).NotTo(HaveOccurred())
-			Expect(link.Attrs().Name).To(Equal(IFNAME))
-			return nil
-		})
-		Expect(err).NotTo(HaveOccurred())
 
-		err = originalNS.Do(func(ns.NetNS) error {
-			defer GinkgoRecover()
+			// Make sure sriov link exists in the target namespace
+			err = targetNs.Do(func(ns.NetNS) error {
+				defer GinkgoRecover()
 
-			err := testutils.CmdDelWithResult(targetNs.Path(), IFNAME, func() error {
-				return cmdDel(args)
-			})
+				link, err := netlink.LinkByName(IFNAME)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(link.Attrs().Name).To(Equal(IFNAME))
+				return nil
+				})
 			Expect(err).NotTo(HaveOccurred())
-			return nil
-		})
-		Expect(err).NotTo(HaveOccurred())
 
-		// Make sure sriov link has been deleted
-		err = targetNs.Do(func(ns.NetNS) error {
-			defer GinkgoRecover()
+			err = originalNS.Do(func(ns.NetNS) error {
+				defer GinkgoRecover()
 
-			link, err := netlink.LinkByName(IFNAME)
-			Expect(err).To(HaveOccurred())
-			Expect(link).To(BeNil())
-			return nil
+				err := testutils.CmdDelWithResult(targetNs.Path(), IFNAME, func() error {
+					return cmdDel(args)
+				})
+				Expect(err).NotTo(HaveOccurred())
+				return nil
+				})
+			Expect(err).NotTo(HaveOccurred())
+
+			// Make sure sriov link has been deleted
+			err = targetNs.Do(func(ns.NetNS) error {
+				defer GinkgoRecover()
+
+				link, err := netlink.LinkByName(IFNAME)
+				Expect(err).To(HaveOccurred())
+				Expect(link).To(BeNil())
+				return nil
+				})
+			Expect(err).NotTo(HaveOccurred())
 		})
-		Expect(err).NotTo(HaveOccurred())
 	})
 })
